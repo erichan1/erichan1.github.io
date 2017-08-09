2db0538765fa3d3b71b34075fe52902e706090e9
erichan1 <30481032+erichan1@users.noreply.github.com>
2017-08-08 17:34:16 -0700

    changed keyboard branch to actual keyboard code

--- scripts/online.js
+++ scripts/online.js
@@ -1,15 +1,19 @@
 /*This is boxgame. There will be a red box on a white background. Walls will move from the right side towards the left. The box can be moved in any direction. If it collides with a box */
 
+var updateInterval = 10; //How many milliseconds between each game update
 var myBox; //equivalent to myGamePiece in the tutorial.
+var myBoxTwo
 var myObstacles;
 var myWalls; 
 var myScore;
+var myKeyboard;
 
-
+//do I need any global vars up here? probably not tbh
 
 //MAIN: starts the game. Calls start() function in myGameArea. Creates a box, walls, and obstacles.
 function startGame() {
     myGameArea.start();
+    myKeyboard = new keyboard();
     myBox = new component(30,30,"red",10,120);
     myWalls = [new component(5,myGameArea.canvas.width,"orange",0,0), //top wall
                new component(myGameArea.canvas.height,5,"orange",0,0), //left wall
@@ -26,16 +30,7 @@ var myGameArea = {
         this.canvas.height = 270;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval=setInterval(updateGameArea, 10); 
-        
-        window.addEventListener('keydown',function(e) {
-            myGameArea.keys= (myGameArea.keys || [] );
-            myGameArea.keys[e.keyCode] = true; 
-        })
-        window.addEventListener('keyup',function(e) {
-            myGameArea.keys[e.keyCode]=false;
-        })
-        
+        this.interval=setInterval(updateGameArea, updateInterval);  
     },
     clear: function() {
         this.context.clearRect(0,0,this.canvas.width,this.canvas.height);
@@ -50,6 +45,7 @@ function component(height,width,color,x,y) {
     this.y=y;
     this.speedX=0;
     this.speedY=0;
+    
     this.update = function() {
         ctx = myGameArea.context;
         ctx.fillStyle=color;
@@ -72,6 +68,7 @@ function component(height,width,color,x,y) {
         this.speedY+=increment;
     }
     this.setSpeedZero = function() {
+        clearInterval(this.accelUpdate);
         this.speedX = 0;
         this.speedY = 0;
     }
@@ -158,58 +155,135 @@ STATIC METHOD:
 interfaces the component with the keyboard. relies on window.addEventListener in myGameArea. 
 sets speed when arrowkeys are pressed. Orientation one. 
 */ 
-function keyboardOne(myComponent) {
-    if(myGameArea.keys && myGameArea.keys[39]) {
-            //myBox.incrementSpeedX(4);
-            myComponent.setSpeedX(1); //makes box go right   
+
+function keyboardOne(myComponent,board) {
+    if(inArray(board.keysPressed,39)) {
+        //myBox.incrementSpeedX(4);
+        myComponent.setSpeedX(1); //makes box go right 
     }
-    if(myGameArea.keys && myGameArea.keys[37]) {
-            //myBox.incrementSpeedX(-4);
-            myComponent.setSpeedX(-1); //makes box go left
+    if(inArray(board.keysPressed,37)) {
+        myComponent.setSpeedX(-1); //makes box go left
     }
-    if(myGameArea.keys && myGameArea.keys[38]) {
-            //myBox.incrementSpeedY(-4);
-            myComponent.setSpeedY(-1); //makes box go up
+    
+    if(inArray(board.keysPressed,38)) {
+        myComponent.setSpeedY(-1); //makes box go up
     }
-    if(myGameArea.keys && myGameArea.keys[40]) {
-            //myBox.incrementSpeedY(4);
-            myComponent.setSpeedY(1); //makes box go down.
+    if(inArray(board.keysPressed,40)) {
+        myComponent.setSpeedY(1); //makes box go down.
     }
 }
+
 
 /*
 STATIC METHOD: 
 interfaces the component with the keyboard. relies on window.addEventListener in myGameArea. 
 sets speed when WASD are pressed. Orientation one. 
 */ 
-function keyboardTwo(myComponent) {
-    if(myGameArea.keys && myGameArea.keys[68]) {
-            //myBox.incrementSpeedX(4);
-            myComponent.setSpeedX(1); //makes box go right   
+
+function keyboardTwo(myComponent,board) {
+
+    if(inArray(board.getKeysPressed,68)) {
+        //myBox.incrementSpeedX(4);
+        myComponent.setSpeedX(1); //makes box go right 
     }
-    if(myGameArea.keys && myGameArea.keys[65]) {
-            //myBox.incrementSpeedX(-4);
-            myComponent.setSpeedX(-1); //makes box go left
+    if(inArray(board.getKeysPressed,65)) {
+        myComponent.setSpeedX(-1); //makes box go left
     }
-    if(myGameArea.keys && myGameArea.keys[87]) {
-            //myBox.incrementSpeedY(-4);
-            myComponent.setSpeedY(-1); //makes box go up
+   
+    
+    if(inArray(board.getKeysPressed,87)) {
+        myComponent.setSpeedY(-1); //makes box go up
     }
-    if(myGameArea.keys && myGameArea.keys[83]) {
-            //myBox.incrementSpeedY(4);
-            myComponent.setSpeedY(1); //makes box go down.
+    if(inArray(board.getKeysPressed,83)) {
+        myComponent.setSpeedY(1); //makes box go down.
     }
+    
+}
+
+
+
+/*
+keyboard class. holds what keys were pressed and released between last iteration. essentially a virtual model of what's going on in real keyboard. created to detect when a key changes from unpressed to pressed. probably already exists somewhere. setInterval has issues. 
+*/
+function keyboard() {
+    
+    this.keys = (this.keys || [] ); //put outside function. don't need to press a key to make keys array.  
+    window.addEventListener('keydown',function(e) {
+        this.keys[e.keyCode] = true; 
+    })
+    window.addEventListener('keyup',function(e) {
+        this.keys[e.keyCode]=false;
+    })
+    
+    this.updateInterval = updateInterval;
+    
+    //contains keyIDs for all letters, arrow keys, space, some others.
+    this.keyIDs = [8,9,13,16,17,18,19,20,27,33,34,35,36,37,38,39,40,45,46,48,49,50,51,52,53,54,55,56,
+                  57,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90];
+    
+    this.keysNow = [];
+    this.keysBefore = [];
+    this.keysPressed = [];
+    this.keysReleased = [];
+    
+    this.keyInterval = setInterval(keyboardUpdate,this.updateInterval); //not sure if this will work.
+    
+    function keyboardUpdate() {
+        
+        //initializes keysNow and puts all keys pressed rn into the array. 
+        this.keysNow = [];
+        this.keysPressed = [];
+        this.keysReleased = [];
+        for(i=0;i<this.keyIDs.length;i++) {
+            if(this.keys[this.keyIDs[i]]==true){
+                this.keysNow.push(this.keyIDs[i]);
+            }      
+        }
+        
+        //compare now and before. 
+        for(i=0;i<this.keysNow.length;i++) {
+            if(!inArray(this.keysBefore,this.keysNow[i])){
+                this.keysPressed.push(this.keysNow[i]) //if something is in keysNow but not keysBefore goes in keysPressed.
+            }
+        }
+        //compare now and before. 
+        for(j=0;j<this.keysBefore.length;j++) {
+            if(!inArray(this.keysNow,this.keysBefore[j])){
+                this.keysReleased.push(this.keysBefore[j]) //if something is in keysBefore but not keysNow goes in keysReleased.
+            }
+        }
+        //copies elements from keysNow to keysBefore; 
+        for(i=0;i<this.keysNow.length;i++) {
+            this.keysBefore.push(this.keyNow[i]);  
+        }
+        console.log("keyBoardUpdate is working"); //trying to check if the function is running repeatedly. 
+    }
+    this.getKeysPressed = function() {
+        return this.keysPressed;
+    }
+    this.getKeysReleased = function() {
+        return this.keysReleased;
+    }
+    
+}
+
+//STATIC METHOD: returns true if an object is in an array and false if not.
+function inArray(array,object) {
+    this.inArr=false; 
+    for(i=0;i<array.length;i++) {
+        if(object==array[i]) {
+            this.inArr = true;
+        }
+    }
+    return this.inArr;
 }
 
 //STATIC METHOD: updates game area. interval shown in var myGameArea.
 function updateGameArea() {
     myGameArea.clear(); //clears game area
-    
-    myBox.setSpeedZero(); //sets speeds to 0
-    myBoxTwo.setSpeedX(0); //sets speeds to 0
-    myBoxTwo.setSpeedY(0);
-    keyboardOne(myBox); //sets speed according to keyboard
-    keyboardTwo(myBoxTwo);
+
+    keyboardOne(myBox,myKeyboard); //sets speed according to keyboard
+    keyboardTwo(myBoxTwo,myKeyboard);
     
     for(i=0;i<myWalls.length;i++) {
         myWalls[i].update();
